5cf79ab1b3540da1098be5d1c81639b93496e12a
arifrahmanto <arifismiadi.rahmanto@mitrais.com>
2025-12-30 00:31:07 +0700

    tambah docs

---
+++ docs/layers/bangunan_4.js
@@ -0,0 +1,8 @@
+var json_bangunan_4 = {
+"type": "FeatureCollection",
+"name": "bangunan_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "nama": "SDN Banjarsari 1" }, "geometry": { "type": "Point", "coordinates": [ 110.743992738509974, -6.89202545548693 ] } }
+]
+}
---
+++ docs/styles/bangunan_4_style.js
@@ -0,0 +1,38 @@
+var size = 0;
+var placement = 'point';
+
+var style_bangunan_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "18.2px \'Open Sans\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "#000000";
+    var bufferWidth = 9.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("nama") !== null) {
+        labelText = String(feature.get("nama"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 8.0 + size, points: 6,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(227,26,28,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 3.8}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 3.2727200000000005 + size, points: 6,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(250,176,124,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(250,176,124,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
